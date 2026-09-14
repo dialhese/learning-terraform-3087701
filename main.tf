f94ac314bcba918317e345fd0ba9f1c2462800af
dialhese <43018484+dialhese@users.noreply.github.com>
2026-09-14 17:31:10 -0600

    use module for security

--- main.tf
+++ main.tf
@@ -26,11 +26,43 @@ resource "aws_instance" "blog" {
   //instance_type = "t3.micro"   # antes: t3.nano
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  //vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.name]
 
   tags = {
     Name = "Learning Terraform"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "6.0.0"
+  name = "blog_new"
+
+
+  vpc_id         = data.aws_vpc.default.id
+  ingress_rules  = {
+    http = {
+      from_port       = 80
+      to_port         = 80
+      ip_protocol     = "tcp"
+      cidr_ipv4       = "0.0.0.0/0"
+    }
+    https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
 }
 
 resource "aws_security_group" "blog" {
