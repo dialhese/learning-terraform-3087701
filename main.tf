aa38fb58d639639e7713003f4042a2a19f111626
dialhese <43018484+dialhese@users.noreply.github.com>
2026-09-14 22:53:42 -0600

    load balancer

--- main.tf
+++ main.tf
@@ -87,6 +87,44 @@ module "blog_sg" {
 
 }
 
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  security_groups = [module.blob_sg.security_group_id]
+
+
+  listeners = {
+    blog-http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_arn = aws_lb_target_group.blog.arn
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_lb_target_group" "blog" {
+  name     = "blog"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "blog" {
+  target_group_arn = aws_lb_target_group.blog.arn
+  target_id        = aws_instance.blog.id
+  port             = 80
+}
+
 //resource "aws_security_group" "blog" {
 //  name        = "blog"
 //  description = "Allow http and https in. Allow everithing out"
