363864cf8dbaa8f27b506909fedb0b8d42bf90a7
dialhese <43018484+dialhese@users.noreply.github.com>
2026-09-18 17:02:08 -0600

    remove attachment

--- main.tf
+++ main.tf
@@ -95,13 +95,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.3.1"
@@ -112,11 +105,11 @@ module "blog_autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
-  launch_template_name = "blog"
+  launch_template_name    = "blog"
   update_default_version  = true
-  security_groups      = [module.blog_sg.id]
-  instance_type        = var.instance_type
-  image_id             = data.aws_ami.app_ami.id
+  security_groups         = [module.blog_sg.id]
+  instance_type           = var.instance_type
+  image_id                = data.aws_ami.app_ami.id
 
 
   user_data = base64encode(<<-EOF
