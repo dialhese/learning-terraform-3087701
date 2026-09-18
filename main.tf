435066f27e9e9872e5d71ea96992b2e3f6ff2155
dialhese <43018484+dialhese@users.noreply.github.com>
2026-09-18 16:49:32 -0600

    lad balancer

--- main.tf
+++ main.tf
@@ -95,11 +95,17 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
+resource "aws_lb_target_group_attachment" "blog" {
+  target_group_arn = aws_lb_target_group.blog.arn
+  target_id        = aws_instance.blog.id
+  port             = 80
+}
+
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.3.1"
-  name = "blog"
+  name    = "blog"
 
   min_size = 1
   max_size = 2
