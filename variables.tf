d36dade871cf2639686999fa71f0eca6cef20ff5
dialhese <43018484+dialhese@users.noreply.github.com>
2026-09-18 17:25:09 -0600

    refactor to use variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,38 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
+
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type = object ({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name  = "al2023-ami-*-x86_64"
+    owner = "137112412989" # Amazon
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type = object ({
+    name = string
+    network_prefix = string
+  })
+  default = '"dev"
+  network_prefix = "10.0"
+}
+
+
+variable "min_size" {
+  description = "Minimum number of instances in the ASG"
+  default = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in the ASG"
+  default = 2
+}
