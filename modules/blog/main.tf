d9d58b0091d622ebc5f68a3f4bab1b4b9af4bc76
dialhese <43018484+dialhese@users.noreply.github.com>
2026-09-18 18:13:01 -0600

    fix alb

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -124,7 +124,7 @@ module "blog_autoscaling" {
   )
 
   traffic_source_attachments = {
-    ${var.environment.name}-blog-alb ={
+    "${var.environment.name}-blog-alb" ={
       traffic_source_identifier = aws_lb_target_group.blog.arn
     }
   }
